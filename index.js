8dac22cfe108eb943ca3eb48ce988add939c7d3d
Kjetil Midtgarden Golid <kjetil.golid@gmail.com>
2020-01-12 15:40:45 +0100

    organize the exports

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-import { get_walk } from './drunk';
+import { flip, shuffle, random_int } from './core';
+import { get_partition } from './partition';
+import { get_walker, get_walk, mutate_walk, snap_to_highlights } from './drunk';
 
-export const drunk_walk = get_walk;
-
-/*
-let walk = get_walk({
-  steps: 100,
-  min_stepsize: 0,
-  max_stepsize: 2,
-  seed: 'hola',
-  allow_standstill: false
-});
-console.log(walk);
-*/
+export {
+  flip,
+  shuffle,
+  random_int,
+  get_partition,
+  get_walker,
+  get_walk,
+  mutate_walk,
+  snap_to_highlights
+};
