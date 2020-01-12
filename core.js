fda7b2d0b668063518e05fdb874f231caf08d819
Kjetil Midtgarden Golid <kjetil.golid@gmail.com>
2020-01-12 15:15:21 +0100

    fix bug where chance = 0 defaults to 0.5

--- core.js
+++ core.js
@@ -1,3 +1,3 @@
 export const random_int = (rng, min, max) => min + Math.floor(rng() * (max - min));
 
-export const flip = (rng, chance) => rng() < (chance ? chance : 0.5);
+export const flip = (rng, chance) => rng() < (chance === undefined ? 0.5 : chance);
