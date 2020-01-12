cb547070995d5c1b237707b3ba3d173184e91f3d
Kjetil Midtgarden Golid <kjetil.golid@gmail.com>
2020-01-12 15:15:49 +0100

    add shuffle method

--- core.js
+++ core.js
@@ -1,3 +1,12 @@
 export const random_int = (rng, min, max) => min + Math.floor(rng() * (max - min));
 
 export const flip = (rng, chance) => rng() < (chance === undefined ? 0.5 : chance);
+
+export const shuffle = (rng, arr) => {
+  const array = arr.slice(0);
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(rng() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
